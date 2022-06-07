58c44b4cd08018976b54c61600e65c8f1c31ed9a
Leticia Harumi <80927546+leticiaharumi@users.noreply.github.com>
2022-06-07 14:46:22 -0300

    Aula 5

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-$('.slider-principal').slick();
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoPlaySpeed: 2000
+});
